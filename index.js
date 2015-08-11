0e15f5c63576e10292493836623e30f8f29aa5d5
Sam Verschueren <sam.verschueren@gmail.com>
2015-08-11 14:33:54 +0200

    First draft of the handler

--- index.js
+++ index.js
@@ -9,14 +9,17 @@
 
 // module dependencies
 var AWS = require('aws-sdk'),
-    //gm = require('gm').subClass({ imageMagick: true }),
-    util = require('util');
+    gm = require('gm').subClass({ imageMagick: true }),
+    mime = require('mime'),
+    util = require('util'),
+    Q = require('q');
 
 // Create a new S3 object
 var s3 = new AWS.S3();
 
 // Generate a 150x150 square image
-var SIZE = 150;
+var SIZE = 150,
+    WHITELIST = ['image/jpeg', 'image/jpg', 'image/png'];
 
 /**
  * Main entrypoint of the service.
@@ -25,7 +28,75 @@ var SIZE = 150;
  * @param {object}  context     The AWS Lambda execution context.
  */
 exports.handler = function(event, context) {
-    console.log(util.inspect(event, {depth: 5}));
+    var bucket = event.Records[0].s3.bucket.name,
+        source = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+        
+    console.log(source);
     
-    context.succeed();
+    // Nodeify the S3 calls
+    var getObject = Q.denodeify(s3.getObject),
+        putObject = Q.denodeify(s3.putObject);
+    
+    Q.fcall(function() {
+        // First test the image type
+        var mimetype = mime.lookup(source);
+        
+        if(WHITELIST.indexOf(mimetype) === -1) {
+            // If the mimetype is not in the whitelist, throw an error
+            throw new TypeError('The type of the file is not correct.');
+        }
+        
+        // Retrieve the object
+        return getObject({Bucket: bucket, Key: source});
+    }).then(function(response) {
+        // Scale and crop the image
+        return [response.ContentType, scale(response.Body)];
+    }).spread(function(contentType, buffer) {
+        var dest = source.split('/');
+        dest.shift();
+        dest.unshift('thumbs');
+        dest = dest.join('/');
+        
+        console.log(dest);
+        
+        // Store the image and the correct location
+        return putObject({Bucket: bucket, Key: dest, Body: buffer, ContentType: contentType});
+    }).then(function() {
+        // Everything went well
+        context.succeed();
+    }).catch(function(err) {
+        // Log the error
+        console.error(err);
+        
+        // Let the function fail
+        context.fail(err);  
+    });
+    
+    function scale(img) {
+        return Q(function(resolve, reject) {
+            // Retrieve the size of the img
+            gm(img).size(function(err, size) {
+                if(err) {
+                    // Reject the promise if an error occurred
+                    return reject(err);
+                }
+                
+                // Calculate the maximum square we can extract
+                var square = Math.min(size.width, size.height), 
+                    x = (size.width / 2) - (square / 2),
+                    y = (size.height / 2) - (square / 2);
+                
+                // Extract the middle square and resize to the SIZE defined
+                gm(img).crop(square, square, x, y).resize(SIZE, SIZE).toBuffer(function(err, buffer) {
+                    if(err) {
+                        // Reject the promise if an error occurred
+                        return reject(err);
+                    }
+                    
+                    // Resolve the buffer if everything went well
+                    resolve(buffer);
+                });
+            });
+        });
+    }
 };
