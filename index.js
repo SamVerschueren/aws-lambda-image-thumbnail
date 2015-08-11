9c01c8a1fd6532e23423a3b0e6da45a32e8549ac
Sam Verschueren <sam.verschueren@gmail.com>
2015-08-11 13:48:33 +0200

    Just inspecting the event object for now

--- index.js
+++ index.js
@@ -8,7 +8,15 @@
  */
 
 // module dependencies
+var AWS = require('aws-sdk'),
+    gm = require('gm').subClass({ imageMagick: true }),
+    util = require('util');
 
+// Create a new S3 object
+var s3 = new AWS.S3();
+
+// Generate a 150x150 square image
+var SIZE = 150;
 
 /**
  * Main entrypoint of the service.
@@ -17,5 +25,7 @@
  * @param {object}  context     The AWS Lambda execution context.
  */
 exports.handler = function(event, context) {
+    console.log(util.inspect(event, {depth: 5}));
+    
     context.succeed();
 };
